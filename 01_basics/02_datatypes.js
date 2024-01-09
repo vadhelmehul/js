21cd857855e623ed14bc003e260a2d5e87a550ce
vadhelmehul <154368656+vadhelmehul@users.noreply.github.com>
2024-01-09 18:47:59 +0000

    datatype

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,1 +1,13 @@
 "use strict" ; // treat all js code as a newer version
+// alert(3 + 3)  we are using node js not browser it not be work as a browser
+
+/*
+
+datatypes in js 
+
+number, string, bigint, boolean, null(standalon value), symbol, undifined, object
+
+*/
+
+console.log(typeof undefined) // undifined
+console.log(typeof null) // object
