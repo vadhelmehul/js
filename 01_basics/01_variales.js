df264d86174edceeb372cd32fb950aec0322910c
vadhelmehul <154368656+vadhelmehul@users.noreply.github.com>
2024-01-09 18:47:38 +0000

    variable

---
+++ 01_basics/01_variales.js
@@ -0,0 +1,14 @@
+const accountId = "12345"
+let accountEmail = "maulik@gmail.com"
+var accountPassword = "123456"
+accountCity = "surat"
+let accountState;
+
+// accountId = 111 // not allowed
+console.log(accountId)
+
+accountEmail = "mm@gmail.com"
+accountPassword = "111111"
+accountCity = "bengaluru"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
