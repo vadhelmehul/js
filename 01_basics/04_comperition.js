a6dbd969a1f82ac57c3cd43b288efa646fea12d2
vadhelmehul <154368656+vadhelmehul@users.noreply.github.com>
2024-01-10 23:57:42 +0530

    Delete 01_basics/04_comperition.js

--- 01_basics/04_comperition.js
+++
@@ -1,4 +0,0 @@
-
-console.log( null > 0)
-console.log( null == 0)
-console.log( null >= 0) // this give true value because at this time null convert its value into number that is the reason behind true value
