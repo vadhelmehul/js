c5611a20befab4e010364c88ab67e5ab02f3828e
vadhelmehul <154368656+vadhelmehul@users.noreply.github.com>
2024-01-10 18:05:50 +0000

    conversion of operator

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,3 +1,5 @@
+/*
+
 let score = 33
 
 console.log(typeof score)
@@ -6,6 +8,8 @@ console.log(typeof (score))
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber)
 
+*/
+
 // 33 => convert into number
 // "33abc" => not convert this will be shown as NaN (not a number)
 // true => convert into 1 or if false => its convert into 0
@@ -13,9 +17,13 @@ console.log(typeof valueInNumber)
 
 // for convert 0/1 into boolean do this 
 
+/*
+
 let isLoggedIn = 1;
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn)
+
+*/
 
 // if we used empty string to convert into boolean it will be given false 
 // if we used any value into string to convert into boolean it will be given true 
@@ -23,6 +31,34 @@ console.log(booleanIsLoggedIn)
 
 // for convert into string do this 
 
+/*
+
 let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber)
+
+*/
+
+/************************************ operations *******************************************/
+
+/*
+
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+*/
+
+/*
+
+console.log(1 + "2")
+console.log("1" + 2)
+console.log("1" + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+*/
+
+let gamecounter = 100
+++gamecounter
+console.log(gamecounter)
