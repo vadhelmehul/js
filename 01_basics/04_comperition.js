1285510c6715b72295ee01b3b2e0bb1c36a9bb69
vadhelmehul <154368656+vadhelmehul@users.noreply.github.com>
2024-01-10 18:21:22 +0000

    comparition.js

---
+++ 01_basics/04_comperition.js
@@ -0,0 +1,4 @@
+
+console.log( null > 0)
+console.log( null == 0)
+console.log( null >= 0) // this give true value because at this time null convert its value into number that is the reason behind true value
