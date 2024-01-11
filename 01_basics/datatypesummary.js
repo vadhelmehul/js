7796b678500d8634b2fde15f412ed5df86db197e
vadhelmehul <154368656+vadhelmehul@users.noreply.github.com>
2024-01-11 16:45:07 +0000

    datatypesummary

--- 01_basics/datatypesummary.js
+++ 01_basics/datatypesummary.js
@@ -4,15 +4,17 @@
 
 // String, number, boolean, null, undefined, symbol, bigint 
 
+/*
 const id = Symbol('123')
 const anotherId = Symbol('123')
 console.log(id === anotherId)
-
+*/
 
 //(2) reference datatypes (non primitive )
 
 // Array, object, function 
 
+/*
 const language = ["gujarati", "hindi", "english"]
 const myObj = {
     name:"maulik",
@@ -21,3 +23,32 @@ const myObj = {
 const myfunction = function(){
     console.log("hello");
 }
+
+*/
+
+
+/*--------------------------------------------------------------------------------------------- */
+
+// stack (primitive) or heap (non primitive)
+
+let myname = "maulik@gmail.com"
+
+let anothername = myname
+anothername = "maulik"
+
+console.log(myname)
+console.log(anothername)
+
+let userOne ={
+    email:"mehul@gmail.com",
+    
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "mehul1@gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
+
