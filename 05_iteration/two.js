bafe11b9d001c2f4ae2e94fdbd3c27ee3eb331eb
vadhelmehul <154368656+vadhelmehul@users.noreply.github.com>
2024-01-23 18:16:29 +0000

    two

---
+++ 05_iteration/two.js
@@ -0,0 +1,27 @@
+
+// while loop 
+
+/*
+let index = 0
+while(index <= 10){
+    console.log(`value of index is ${index}`)
+    index = index + 2
+}
+*/
+
+/*
+let myarr= ['batman', 'superman', 'flash']
+let arr= 0
+while(arr < myarr.length){
+    console.log(`value is ${myarr[arr]}`)
+    arr = arr + 1
+}
+
+*/
+
+let score = 1
+
+do{
+    console.log(`score is ${score}`)
+    score++
+}while(score <= 10)
