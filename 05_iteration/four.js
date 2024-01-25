0ef0eb4a85eaa0439359d6ed266f8c0ebb0f26d1
vadhelmehul <154368656+vadhelmehul@users.noreply.github.com>
2024-01-25 16:36:03 +0000

    four

---
+++ 05_iteration/four.js
@@ -0,0 +1,16 @@
+const language ={
+    js : "javascript",
+    ruby : "ruby",
+    cpp : "c++"
+}
+
+for(const key in language){
+    // console.log(`${key} shortcut is for ${language[key]}`)
+}
+
+const programming = ["js", "rb", "java","py","c language","swift","c#"]
+for(const key in programming){
+    // console.log(key)
+    // console.log(programming[key])
+    // console.log(key,"=", programming[key])
+}
